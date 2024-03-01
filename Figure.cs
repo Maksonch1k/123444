f0cfb585d10cdc8861fad2f49bab076e4e948865
Maksonch1k <123887520+Maksonch1k@users.noreply.github.com>
2024-03-01 11:37:59 +0200

    Add files via upload

---
+++ Figure.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Net;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public abstract class Figure
+    {
+
+    
+        private string _name;
+
+        public Figure(string name)
+        {
+            _name = name;
+        }
+
+        public string Name => _name;
+
+        public abstract double Area();
+        public abstract double Perimeter();
+
+        public virtual void Print()
+        {
+            Console.WriteLine($"Figure: {_name}");
+        }
+    }
+    
+
+
+    
+
+
+
+
+    }
+
